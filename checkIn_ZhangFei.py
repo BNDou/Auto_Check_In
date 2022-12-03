478a3924805f6d7fc3a55ec318862c09540157c3
BNDou <67632799+BNDou@users.noreply.github.com>
2022-12-03 20:45:19 +0800

    更新 checkIn_ZhangFei.py

--- checkIn_ZhangFei.py
+++ checkIn_ZhangFei.py
@@ -3,9 +3,9 @@ new Env('掌上飞车签到')
 cron: 1 0 * * *
 Author       : BNDou
 Date         : 2022-12-02 19:03:27
-LastEditTime : 2022-12-02 20:17:47
-FilePath     : \Auto_Check_In\checkIn_ZhangFei.py
-Description  : 添加环境变量COOKIE_ZHANGFEI、URL_ZHANGFEI、REFERER_ZHANGFEI
+LastEditTime : 2022-12-03 20:44:02
+FilePath     : /Auto_Check_In/checkIn_ZhangFei.py
+Description  : 添加环境变量COOKIE_ZHANGFEI、URL_ZHANGFEI
 '''
 from lxml import etree
 import requests
@@ -23,7 +23,6 @@ except:
 # 获取环境变量
 cookie_zhangfei = os.environ.get("COOKIE_ZHANGFEI")
 url_zhangfei = os.environ.get("URL_ZHANGFEI")
-referer_zhangfei = os.environ.get("REFERER_ZHANGFEI")
 
 
 def load_send():
@@ -44,48 +43,49 @@ def load_send():
 load_send()
 
 
-def run(*arg):
+def run(cookie, url):
     msg = ""
     s = requests.Session()
     s.headers.update({'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 GH_QQConnect GameHelper_1003/2103040778'})
 
     # 签到
-    url = url_zhangfei
     headers = {
-        'Host': 'mwegame.qq.com',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 GH_QQConnect GameHelper_1003/2103040778',
         'Connection': 'keep-alive',
         'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 GH_QQConnect GameHelper_1003/2103040778',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': referer_zhangfei,
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Cookie': cookie_zhangfei,
+        'Cookie': cookie
     }
 
     r = s.get(url, headers=headers, timeout=120)
     rjson = r.json()
-    msg += rjson['message']
+    msg += rjson.get('message', '')
+    if 'send_result' in rjson:
+        msg += '\n' + rjson.get('send_result').get('sMsg', '')
 
-    return msg + '\n'
+    return msg + '\n\n'
 
 
 def main(*arg):
     msg = ""
     sendnoty = 'true'
     global cookie_zhangfei
-    if "\\n" in cookie_zhangfei:
-        clist = cookie_zhangfei.split("\\n")
+    global url_zhangfei
+    if '\n' in cookie_zhangfei:
+        clist = cookie_zhangfei.split('\n')
     else:
         clist = cookie_zhangfei.split("\n")
+    if '\n' in url_zhangfei:
+        ulist = url_zhangfei.split('\n')
+    else:
+        ulist = url_zhangfei.split("\n")
     i = 0
     while i < len(clist):
         msg += f"第 {i+1} 个账号开始执行任务\n"
-        cookie_zhangfei = clist[i]
-        msg += run(cookie_zhangfei)
+        cookie_zhangfei = clist[i].replace(' ', '')
+        url_zhangfei = ulist[i].replace(' ', '')
+        msg += run(cookie_zhangfei, url_zhangfei)
         i += 1
     print(msg[:-1])
     if sendnoty:
