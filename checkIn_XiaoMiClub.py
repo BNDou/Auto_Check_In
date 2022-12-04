84f153792bda2ef7b05710c3501cbae65556a1b4
BNDou <67632799+BNDou@users.noreply.github.com>
2022-12-04 21:47:14 +0800

    更新 checkIn_XiaoMiClub.py

--- checkIn_XiaoMiClub.py
+++ checkIn_XiaoMiClub.py
@@ -3,7 +3,7 @@ new Env('小米社区日常')
 cron: 1 0 * * *
 Author       : BNDou
 Date         : 2022-12-03 16:58:45
-LastEditTime : 2022-12-04 21:06:02
+LastEditTime : 2022-12-04 21:32:45
 FilePath     : /Auto_Check_In/checkIn_XiaoMiClub.py
 Description  : 
 添加环境变量COOKIE_XIAOMICLUB，多账号用回车换行分开
@@ -82,7 +82,7 @@ def run(cookie, url):
     a = r.json()
 
     if 'code' in a:
-        if (401, 500) == a.get('code'):
+        if a.get('code') in (401, 500):
             print(
                 '失败，可能是cookie失效了\n建议手机端访问签到页面时抓cookie，其包含字段：miui_vip_serviceToken、cUserId、userId')
             send(
