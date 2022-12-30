b31ed71f8de48165f5a9c67fa337c05ad912b30d
BNDou <67632799+BNDou@users.noreply.github.com>
2022-12-30 23:30:30 +0800

    更新 checkIn_test.py

--- checkIn_test.py
+++ checkIn_test.py
@@ -3,7 +3,7 @@ new Env('测试TEST')
 cron: 11 11 11 * *
 Author       : BNDou
 Date         : 2022-12-02 19:03:27
-LastEditTime : 2022-12-30 22:54:31
+LastEditTime : 2022-12-30 23:30:05
 FilePath     : /Auto_Check_In/checkIn_test.py
 Description  : 
 '''
@@ -19,20 +19,6 @@ requests.packages.urllib3.disable_warnings()
 # os.environ['COOKIE_ZHANGFEI'] = 'ssssss'
 # os.environ['REFERER_ZHANGFEI'] = 'qqqqqq'
 # os.environ['REFERER_ZHANGFEI'] = 'wwwwww'
-
-
-# 返回值 list[jd_cookie]
-def get_ck():  # 方法 获取 JD_COOKIE值 [系统变量传递] <! 此方法未使用 !>
-    if "JD_COOKIE" in os.environ:  # 判断 JD_COOKIE是否存在于环境变量
-        ck_list = os.environ['JD_COOKIE'].split('&')  # 读取系统变量 以 & 分割变量
-        if len(ck_list) > 0:  # 判断 WSKEY 数量 大于 0 个
-            return ck_list  # 返回 JD_COOKIE [LIST]
-        else:  # 判断分支
-            print("JD_COOKIE变量未启用")  # 标准日志输出
-            sys.exit(1)  # 脚本退出
-    else:  # 判断分支
-        print("未添加JD_COOKIE变量")  # 标准日志输出
-        sys.exit(0)  # 脚本退出
 
 
 # 获取环境变量
@@ -72,16 +58,13 @@ def main(*arg):
     global cookie_zhangfei
     global referer_zhangfei
     cookie_zhangfei, referer_zhangfei = get_env()
-    jdcookies = get_ck()
 
-    print(jdcookies)
     print(cookie_zhangfei)
+    print('\n')
     print(referer_zhangfei)
 
     return msg[:-1]
 
 
 if __name__ == "__main__":
-    print("----------掌上飞车开始尝试开金丝篓----------")
     main()
-    print("----------掌上飞车开金丝篓执行完毕----------")
