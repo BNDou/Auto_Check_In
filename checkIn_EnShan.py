3daf9d462c6c8bc2ae1709d962d09cc1518e692d
BNDou <942490898@qq.com>
2024-04-14 00:50:45 +0800

    类封装优化代码

--- checkIn_EnShan.py
+++ checkIn_EnShan.py
@@ -3,12 +3,13 @@ new Env('恩山论坛签到')
 cron: 1 0 * * *
 Author       : BNDou
 Date         : 2022-10-30 22:21:48
-LastEditTime : 2024-03-03 15:16:35
+LastEditTime : 2024-04-14 00:31:35
 FilePath     : /Auto_Check_In/checkIn_EnShan.py
-Description  : 添加环境变量COOKIE_ENSHAN，多账号用回车换行分开
+Description  : 添加环境变量COOKIE_ENSHAN，多账号用 回车 或 && 分开
 '''
 
 import os
+import re
 import sys
 
 import requests
@@ -27,15 +28,8 @@ except Exception as err:  # 异常捕捉
 def get_env():
     # 判断 COOKIE_ENSHAN是否存在于环境变量
     if "COOKIE_ENSHAN" in os.environ:
-        # 读取系统变量 以 \n 分割变量
-        cookie_list = os.environ.get('COOKIE_ENSHAN').split('\n')
-        # 判断 cookie 数量 大于 0 个
-        if len(cookie_list) <= 0:
-            # 标准日志输出
-            print('COOKIE_ENSHAN变量未启用')
-            send('恩山论坛签到', 'COOKIE_ENSHAN变量未启用')
-            # 脚本退出
-            sys.exit(0)
+        # 读取系统变量以 \n 或 && 分割变量
+        cookie_list = re.split('\n|&&', os.environ.get('COOKIE_ENSHAN'))
     else:
         # 标准日志输出
         print('未添加COOKIE_ENSHAN变量')
@@ -46,38 +40,54 @@ def get_env():
     return cookie_list
 
 
-def run(cookie):
-    msg = ""
+class EnShan:
+    def __init__(self, cookie):
+        self.cookie = cookie
+        self.user_name = None
+        self.user_group = None
+        self.coin = None
+        self.contribution = None
+        self.point = None
+        self.date = None
 
-    # 签到
-    url = "https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit&op=log&suboperation=creditrulelog"
-    try:
-        r = requests.get(url=url, headers={'Cookie': cookie}, timeout=120)
-        # print(r.text)
-        if '每天登录' in r.text:
-            h = etree.HTML(r.text)
+    def get_user(self):
+        """获取用户积分"""
+        user_url = "https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit"
+        user_res = requests.get(url=user_url, headers={'Cookie': self.cookie})
+        self.user_name = re.findall(r'访问我的空间">(.*?)</a>', user_res.text)[0]
+        self.user_group = re.findall(r'用户组: (.*?)</a>', user_res.text)[0]
+        self.contribution = re.findall(r'贡献: </em>(.*?) 分', user_res.text)[0]
+        self.coin = re.findall(r'恩山币: </em>(.*?) 币', user_res.text)[0]
+        self.point = re.findall(r'积分: </em>(.*?) ', user_res.text)[0]
 
-            user = h.xpath('//strong[@class="vwmy qq"]//a/text()')[0]
-            date = h.xpath('//tr/td[6]/text()')[0]
-            money = h.xpath('//a[@id="extcreditmenu"]/text()')[0]
+    def get_log(self):
+        """获取签到日期记录"""
+        log_url = "https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit&op=log&suboperation=creditrulelog"
+        log_res = requests.get(url=log_url, headers={'Cookie': self.cookie})
+        html = etree.HTML(log_res.text)
+        self.date = html.xpath('//tr/td[6]/text()')[0]
 
-            msg += f'👶账号：{user}\n🏅{money}\n⭐签到成功或今日已签到\n⭐最后签到时间：{date}'
+    def main(self):
+        """执行"""
+        self.get_log()
+        self.get_user()
+
+        if self.date:
+            return f'👶{self.user_group}：{self.user_name}\n🏅恩山币：{self.coin} 贡献：{self.contribution} 积分：{self.point}\n⭐签到成功或今日已签到\n⭐最后签到时间：{self.date}'
         else:
-            msg += '❌️签到失败，可能是cookie失效了！'
-    except:
-        msg = '❌️无法正常连接到网站，请尝试改变网络环境，试下本地能不能跑脚本，或者换几个时间点执行脚本'
-
-    return msg + '\n\n'
+            return '❌️签到失败，可能是cookie失效了！'
 
 
-def main(*arg):
-    msg, cookie_enshan = "", get_env()
+if __name__ == "__main__":
+    print("----------恩山论坛开始尝试签到----------")
+
+    msg, cookie_EnShan = "", get_env()
 
     i = 0
-    while i < len(cookie_enshan):
+    while i < len(cookie_EnShan):
         log = f"第 {i + 1} 个账号开始执行任务\n"
-        log += run(cookie_enshan[i])
-        msg += log
+        log += EnShan(cookie_EnShan[i]).main()
+        msg += log + "\n\n"
         print(log)
         i += 1
 
@@ -86,10 +96,4 @@ def main(*arg):
     except Exception as err:
         print('%s\n❌️错误，请查看运行日志！' % err)
 
-    return msg[:-1]
-
-
-if __name__ == "__main__":
-    print("----------恩山论坛开始尝试签到----------")
-    main()
     print("----------恩山论坛签到执行完毕----------")
