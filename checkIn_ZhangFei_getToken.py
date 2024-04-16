79397030ea9cf619fcd6fc8e3a4ab3d5a04150da
BNDou <942490898@qq.com>
2024-04-17 03:19:04 +0800

    更新 checkIn_ZhangFei_getToken.py

--- checkIn_ZhangFei_getToken.py
+++ checkIn_ZhangFei_getToken.py
@@ -27,7 +27,7 @@ cron: 1 1 1 1 1
 
 Author: BNDou
 Date: 2024-04-11 22:20:35
-LastEditTime: 2024-04-17 03:15:47
+LastEditTime: 2024-04-17 03:18:57
 FilePath: \Auto_Check_In\checkIn_ZhangFei_getToken.py
 Description: 
 '''
@@ -104,7 +104,6 @@ if __name__ == "__main__":
 
     # 4、获取产生p_skey的url
     url = re.search(r"ptuiCB\('0','0','(.*?)'", res_login.text).group(1)
-    # url = re.search(r"ptuiCB\('0','0','(.*?)'", "ptuiCB('0','0','http://ptlogin4.openmobile.qq.com/check_sig?pttype=1&uin=942490898&service=ptqrlogin&nodirect=0&ptsigx=3897d16a5a8d5aba30215b0d05c7da4e7a8bee0f6e00ebfd69fda242ee83cd4c6f3cb074eb806b384d2dda8f8a53769f5c438dd645b18fe133515be84439d888f3fc577c7a5de820e8f0185ee3e56ca7&s_url=http%3A%2F%2Fconnect.qq.com&f_url=&ptlang=2052&ptredirect=100&aid=716027609&daid=381&j_later=0&low_login_hour=0&regmaster=0&pt_login_type=3&pt_aid=0&pt_aaid=16&pt_light=0&pt_3rd_aid=1105330667','0','登录成功！', 'Mr.Bean', '')").group(1)
     # print(url)
     # 遍历 cookies 字典并拼接成 cookie 格式
     res_check_sig = requests.get(
