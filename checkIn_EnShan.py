35b05ba9bc82bc4209b57bdc9d3a322c0d15de44
BNDou <942490898@qq.com>
2024-10-08 23:11:20 +0800

    更新 checkIn_EnShan.py

--- checkIn_EnShan.py
+++ checkIn_EnShan.py
@@ -3,9 +3,10 @@ new Env('恩山论坛签到')
 cron: 1 0 * * *
 Author       : BNDou
 Date         : 2022-10-30 22:21:48
-LastEditTime: 2024-08-03 20:45:54
+LastEditTime: 2024-10-08 23:11:05
 FilePath: \Auto_Check_In\checkIn_EnShan.py
 Description  : 添加环境变量COOKIE_ENSHAN，多账号用 回车 或 && 分开
+    测试cookie只需要 rHEX_2132_saltkey 和 rHEX_2132_auth 这俩值，如果报错，试试复制全部cookie
 '''
 
 import os
@@ -53,7 +54,7 @@ class EnShan:
     def get_user(self):
         """获取用户积分"""
         user_url = "https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit"
-        user_res = requests.get(url=user_url, headers={'Cookie': self.cookie})
+        user_res = requests.get(url=user_url, headers={'Cookie': self.cookie, 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;)'})
         self.user_name = re.findall(r'访问我的空间">(.*?)</a>', user_res.text)[0]
         self.user_group = re.findall(r'用户组: (.*?)</a>', user_res.text)[0]
         self.contribution = re.findall(r'贡献: </em>(.*?) 分', user_res.text)[0]
@@ -63,7 +64,7 @@ class EnShan:
     def get_log(self):
         """获取签到日期记录"""
         log_url = "https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit&op=log&suboperation=creditrulelog"
-        log_res = requests.get(url=log_url, headers={'Cookie': self.cookie})
+        log_res = requests.get(url=log_url, headers={'Cookie': self.cookie, 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;)'})
         html = etree.HTML(log_res.text)
         self.date = html.xpath('//tr/td[6]/text()')[0]
 
